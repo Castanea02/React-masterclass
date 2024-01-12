11568ab93f87828478bf2929d520ae22d0ee14e0
Castanea02 <ahrhk2000@gmail.com>
2024-01-12 17:22:55 +0900

    Variants

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,75 @@
-import { createGlobalStyle } from "styled-components";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import styled from "styled-components";
-import { toDoState } from "./atoms";
-import Board from "./components/Board";
+import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
-  display: flex;
-  max-width: 480px;
-  margin: 0 auto;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
   height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Boards = styled.div`
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
   display: grid;
-  width: 100%;
-  grid-template-columns: repeat(3, 1fr);
-  gap: 10px;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  text-align: center;
+  justify-content: center;
+  align-items: center;
 `;
+
+const Circle = styled(motion.div)`
+  width: 70px;
+  height: 70px;
+  place-self: center;
+  background-color: white;
+  border-radius: 50px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const circleVariants = {
+  start: {
+    opacity: 0,
+    y: 10,
+  },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
+};
 
 function App() {
-  const [toDos, setToDos] = useRecoilState(toDoState);
-
-  const onDragEnd = (info: DropResult) => {
-    const { destination, draggableId, source } = info;
-
-    if (!destination) return;
-    if (destination?.droppableId === source.droppableId) {
-      //같은 공간 보드에서 움직임
-      setToDos((allBoards: any) => {
-        const boardCopy = [...allBoards[source.droppableId]];
-        const taskObj = boardCopy[source.index];
-        boardCopy.splice(source.index, 1);
-        boardCopy.splice(destination.index, 0, taskObj);
-        return {
-          ...allBoards,
-          [source.droppableId]: boardCopy,
-        };
-      });
-    }
-
-    if (destination?.droppableId !== source.droppableId) {
-      //다른 공간으로 움직임
-      setToDos((allBoards) => {
-        const sourceBoard = [...allBoards[source.droppableId]];
-        const taskObj = sourceBoard[source.index];
-        const destinationBoard = [...allBoards[destination.droppableId]];
-        sourceBoard.splice(source.index, 1);
-        destinationBoard.splice(destination?.index, 0, taskObj);
-
-        return {
-          ...allBoards,
-          [source.droppableId]: sourceBoard,
-          [destination.droppableId]: destinationBoard,
-        };
-      });
-    }
-  };
-
   return (
-    <DragDropContext onDragEnd={onDragEnd}>
-      <Wrapper>
-        <Boards>
-          {Object.keys(toDos).map((boardId) => (
-            <Board
-              boardId={boardId}
-              key={boardId}
-              toDos={toDos[boardId]}></Board>
-          ))}
-        </Boards>
-      </Wrapper>
-    </DragDropContext>
+    <Wrapper>
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+        <Circle variants={circleVariants} />
+      </Box>
+    </Wrapper>
   );
 }
 
