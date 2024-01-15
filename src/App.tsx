23d4f1ba3a9308499def83ace3ab453b75c7ab9b
Castanea02 <ahrhk2000@gmail.com>
2024-01-16 03:43:28 +0900

    [Netfilx Clone] Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,84 +1,25 @@
-import styled from "styled-components";
-import {
-  motion,
-  useMotionValue,
-  useMotionValueEvent,
-  useScroll,
-  useTransform,
-  AnimatePresence,
-} from "framer-motion";
-import { useEffect, useRef, useState } from "react";
-import { click } from "@testing-library/user-event/dist/click";
-import { exit } from "process";
-
-const Wrapper = styled(motion.div)`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
-`;
-
-const Box = styled(motion.div)`
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  top: 100px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-
-const Grid = styled(motion.div)`
-  display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  width: 50vh;
-  gap: 10px;
-  div:first-child,
-  div:last-child {
-    grid-column: span 2;
-  }
-`;
-
-const Overlay = styled(motion.div)`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const boxVariants = {};
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+import Header from "./Components/Header";
 
 function App() {
-  const [id, setId] = useState<null | string>(null);
-  console.log(id);
-
   return (
-    <Wrapper>
-      <Grid>
-        {["1", "2", "3", "4"].map((n) => (
-          <Box onClick={() => setId(n)} key={n} layoutId={n} />
-        ))}
-      </Grid>
-      <AnimatePresence>
-        {id ? (
-          <Overlay
-            onClick={() => setId(null)}
-            initial={{ backgroundColor: "rgba(0,0,0,0)", opacity: 0 }}
-            animate={{ backgroundColor: "rgba(0,0,0,1)", opacity: 1 }}
-            exit={{ backgroundColor: "rgba(0,0,0,0)", opacity: 0 }}>
-            <Box layoutId={id} style={{ width: 600, height: 200 }}>
-              It working
-            </Box>
-          </Overlay>
-        ) : null}
-      </AnimatePresence>
-    </Wrapper>
+    <Router>
+      <Header></Header>
+      <Switch>
+        <Route path="/tv">
+          <Tv></Tv>
+        </Route>
+        <Route path="/search">
+          <Search></Search>
+        </Route>
+        <Route path="/">
+          <Home></Home>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
